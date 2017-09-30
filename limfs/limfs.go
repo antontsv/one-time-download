1ef9e2f2e4ab0015a7cf9aa3ce3ee0cd0ab524c4
Anton Tsviatkou <a.tsviatkou@gmail.com>
2017-09-29 21:50:13 -0700

    More limited handler into its own package

---
+++ limfs/limfs.go
@@ -0,0 +1,86 @@
+// Package limfs provides a functionality of static file server
+// with enforces a limit on maximum number of downloads
+package limfs
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// LimitedHandler gives ability to limit number of downloads
+// for static file server
+type LimitedHandler struct {
+	dirPath         string
+	fs              http.Handler
+	filesAccessed   map[string]*int
+	maxTimesPerFile int
+}
+
+// New creates a file server handler with pre-defined max number of downloads
+func New(fileDir string, maxDownloadsPerFile int) *LimitedHandler {
+	return &LimitedHandler{
+		dirPath:         fileDir,
+		fs:              http.FileServer(http.Dir(fileDir)),
+		filesAccessed:   make(map[string]*int),
+		maxTimesPerFile: maxDownloadsPerFile,
+	}
+}
+
+func respondWithMessage(w http.ResponseWriter, msg string, httpStatus int) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(httpStatus)
+	_, err := w.Write([]byte(fmt.Sprintf("<center><h1>%s</h1></center>", msg)))
+	if err != nil {
+		log.Printf("Error sending HTML response: %v", err)
+	}
+}
+
+func (ltdHandler *LimitedHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Print(r)
+	v := ltdHandler.timesAccessed(r.RequestURI)
+	if v == nil {
+		respondWithMessage(w, "Not Found!", http.StatusNotFound)
+		return
+	}
+	if *v >= ltdHandler.maxTimesPerFile {
+		respondWithMessage(w, "File is no longer available for download", http.StatusGone)
+		return
+	}
+	if r.Method == http.MethodGet {
+		(*v)++
+		w.Header().Add("X-Remaining-Times", strconv.Itoa(ltdHandler.maxTimesPerFile-*ltdHandler.timesAccessed(r.RequestURI)))
+		ltdHandler.fs.ServeHTTP(w, r)
+	}
+
+}
+
+func (ltdHandler *LimitedHandler) timesAccessed(path string) *int {
+	if !strings.HasPrefix(path, "/") {
+		path = "/" + path
+	}
+	requestedFile := fmt.Sprintf("%s%s", ltdHandler.dirPath, path)
+	if _, err := os.Stat(requestedFile); !os.IsNotExist(err) {
+		v, ok := ltdHandler.filesAccessed[requestedFile]
+		if !ok {
+			var init = 0
+			ltdHandler.filesAccessed[requestedFile] = &init
+			v = &init
+		}
+		return v
+	}
+	return nil
+}
+
+//DisallowAccess blocks access to speficied file
+func (ltdHandler *LimitedHandler) DisallowAccess(path string) bool {
+	v := ltdHandler.timesAccessed(path)
+	if v != nil {
+		*v = ltdHandler.maxTimesPerFile
+		return true
+	}
+	return false
+}
--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/antontsv/one-time-download/limfs"
 )
 
-const fileDir = "files"
-const maxTimeToAllowDownload = 1
+const (
+	defaultFileDir          = "files"
+	defaultMaxDownloadTimes = 1
+	defaultBindAddress      = "localhost:8080"
+	bindAddressEnv          = "BIND_ADDRESS"
+)
 
 func getAddress(defaultAddress string) string {
-	addrStr := os.Getenv("BIND_ADDRESS")
+
+	addrStr := os.Getenv(bindAddressEnv)
 	parts := strings.Split(addrStr, ":")
 
 	if len(parts) == 2 {
@@ -27,76 +35,23 @@ func getAddress(defaultAddress string) string {
 }
 
 func main() {
-	limitedNumberHandler := &oneTimeStaticHandler{
-		fs:            http.FileServer(http.Dir(fileDir)),
-		filesAccessed: make(map[string]*int),
-	}
-	limitedNumberHandler.disallowAccess("README.md")
 
-	http.Handle("/", limitedNumberHandler)
-	address := getAddress("localhost:8080")
+	bindAddr := flag.String("bind_address", defaultBindAddress,
+		fmt.Sprintf(`address and port that server should be listening on. 
+	Can be also set using %s env variable`, bindAddressEnv))
+	fileDir := flag.String("file_dir", defaultFileDir, "path to directory with files to be served")
+	maxTimes := flag.Int("max_times", defaultMaxDownloadTimes, "max times to allow download of a particular file")
+
+	flag.Parse()
+
+	limitedHandler := limfs.New(*fileDir, *maxTimes)
+	limitedHandler.DisallowAccess("README.md")
+
+	http.Handle("/", limitedHandler)
+	address := getAddress(*bindAddr)
 	log.Printf("Starting server on %s...\n", address)
 	err := http.ListenAndServe(address, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-type oneTimeStaticHandler struct {
-	fs            http.Handler
-	filesAccessed map[string]*int
-}
-
-func respondWithMessage(w http.ResponseWriter, msg string, httpStatus int) {
-	w.Header().Add("Content-Type", "text/html")
-	w.WriteHeader(httpStatus)
-	_, err := w.Write([]byte(fmt.Sprintf("<center><h1>%s</h1></center>", msg)))
-	if err != nil {
-		log.Printf("Error sending HTML response: %v", err)
-	}
-}
-
-func (oneTime *oneTimeStaticHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Print(r)
-	v := oneTime.timesAccessed(r.RequestURI)
-	if v == nil {
-		respondWithMessage(w, "Not Found!", http.StatusNotFound)
-		return
-	}
-	if *v >= maxTimeToAllowDownload {
-		respondWithMessage(w, "File is no longer available for download", http.StatusGone)
-		return
-	}
-	if r.Method == http.MethodGet {
-		(*v)++
-		w.Header().Add("X-Download-Times", strconv.Itoa(*oneTime.timesAccessed(r.RequestURI)))
-		oneTime.fs.ServeHTTP(w, r)
-	}
-
-}
-
-func (oneTime *oneTimeStaticHandler) timesAccessed(path string) *int {
-	if !strings.HasPrefix(path, "/") {
-		path = "/" + path
-	}
-	requestedFile := fmt.Sprintf("%s%s", fileDir, path)
-	if _, err := os.Stat(requestedFile); !os.IsNotExist(err) {
-		v, ok := oneTime.filesAccessed[requestedFile]
-		if !ok {
-			var init = 0
-			oneTime.filesAccessed[requestedFile] = &init
-			v = &init
-		}
-		return v
-	}
-	return nil
-}
-
-func (oneTime *oneTimeStaticHandler) disallowAccess(path string) bool {
-	v := oneTime.timesAccessed(path)
-	if v != nil {
-		*v = maxTimeToAllowDownload
-		return true
-	}
-	return false
 }
